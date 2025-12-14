579b7164f89ecf54aa318ac36bd95889f0a4d6f9
Ingrid Mattos <ingrid_matt@hotmail.com>
2025-12-13 21:14:26 -0300

    acrescentando gitignore

--- Application/obj/Release/net9.0/PrecificacaoConfeitaria.Application.AssemblyInfo.cs
+++ Application/obj/Release/net9.0/PrecificacaoConfeitaria.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96af13a736a1e0a2250fd79113aa43fd025db66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd953542bbf83db300d8e3f31c51687aa415e1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
